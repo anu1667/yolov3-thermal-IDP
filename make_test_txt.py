73d23e205f0afe8673e9ef340a08b26dad8b6522
anu1667 <48350911+anu1667@users.noreply.github.com>
2020-05-11 11:38:04 +0530

    Delete make_test_txt.py

--- make_test_txt.py
+++
@@ -1,8 +0,0 @@
-count = 10000
-
-with open("/home/anandita/darknet/data/train_new.txt" , 'w') as f:
-
-	while(count <= 10228):
-		f.write("/home/anandita/MEGAsync Downloads/FLIR/FLIR_ADAS/validation/PreviewData/FLIR_" + str(count) + ".jpeg" +'\n')
-		count +=1
-
